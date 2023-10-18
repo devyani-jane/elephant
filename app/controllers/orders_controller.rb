7e8bfaea54f67a50ccf42c1fc93439e9d3641057
Devyani Rohilla <devyani.rohilla@jane.app>
2023-10-18 12:00:46 -0700

    calculate order value

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,12 @@
 class OrdersController < ApplicationController
   def index
   end
+
+  def create
+    @items_count = params["items_count"]
+    @item_price = params["item_price"]
+    @total = @items_count.to_i * @item_price.to_f
+
+    render :index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htm
 
-  resources :orders, only: [:index]
+  resources :orders, only: [:index, :create]
   root "orders#index"
 end
